39d88efed9a2e6fc5aa8475940eaeb6724840b39
mxiaozz <mxiaozz@qq.com>
2023-11-22 08:28:27 +0800

    monitor

--- bin/stats.sh
+++ bin/stats.sh
@@ -2,7 +2,9 @@
 
 log_date=`date +"%Y-%m-%d %H:%M:%S"`
 
-db_path="/etc/openvpn/ovpn_users.db"
+# chmod 777 sqlite3.db
+# chmod 777 /etc/openvpn
+db_path="/etc/openvpn/sqlite3.db"
 
 sql_exec() {
     rst=`echo $1 | sqlite3 $db_path`
@@ -13,7 +15,7 @@ sql_exec() {
 }
 
 if [[ -z "$bytes_received" ]];then
-    sql_exec "insert into ovpn_users(user_name, ipaddr, login_location, status, msg, login_time) values('"$username"', '"$untrusted_ip"', 'openvpn', '1', 'connected', '"$log_date"');"
+    sql_exec "insert into sys_logininfor(user_name, ipaddr, login_location, browser, status, msg, login_time) values('$username', '$untrusted_ip', 'openvpn', '$ifconfig_pool_local_ip', '0', 'connected', '$log_date');"
 else
-    sql_exec "insert into ovpn_users(user_name, ipaddr, login_location, status, msg, login_time) values('"$username"', '"$untrusted_ip"', 'openvpn', '1', 'disconnected', '"$log_date"');"
+    sql_exec "insert into sys_logininfor(user_name, ipaddr, login_location, browser, os, status, msg, login_time) values('$username', '$untrusted_ip', 'openvpn', '$bytes_received', '$bytes_sent', '0', 'disconnected', '$log_date');"
 fi
