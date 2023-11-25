5c9204ea6fe5a9353cdf1cc42b902c9ed6d8efbd
mxiaozz <mxiaozz@qq.com>
2023-11-25 15:33:05 +0800

    优化执行 shell 脚本

--- bin/checkname.sh
+++ bin/checkname.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$common_name" == "$username" ]];then
+if [ "$common_name" == "$username" ]; then
 	exit 0
 else
 	echo "cn: $common_name 与 username: $username 不一致"
--- bin/stats.sh
+++ bin/stats.sh
@@ -9,25 +9,18 @@ if [ -z "$OPENVPN" ]; then
 fi
 db_path="$OPENVPN/sqlite3.db"
 
-attr=`ls -ld1 $OPENVPN | awk '{print $1}'`
-if [ "$attr" != "drwxrwxrwx" ]; then
-    chmod 777 $OPENVPN
-fi
-attr=`ls -l1 $db_path | awk '{print $1}'`
-if [ "$attr" != "-rwxrwxrwx" ]; then
-    chmod 777 $db_path
-fi
-
 sql_exec() {
     rst=`echo $1 | sqlite3 $db_path`
-    if [[ $? -ne 0 ]];then
+    if [ $? -ne 0 ]; then
         exit 1
     fi
     echo $rst
 }
 
-if [[ -z "$bytes_received" ]];then
-    sql_exec "insert into sys_logininfor(user_name, ipaddr, login_location, browser, os, status, msg, login_time) values('$username', '$untrusted_ip', 'openvpn', '$ifconfig_pool_remote_ip', '$ifconfig_pool_local_ip', '0', 'connected', '$log_date');"
-else
-    sql_exec "insert into sys_logininfor(user_name, ipaddr, login_location, browser, os, status, msg, login_time) values('$username', '$untrusted_ip', 'openvpn', '[send] $bytes_received', '[recv] $bytes_sent', '0', 'disconnected', '$log_date');"
+if [ -f "$db_path" ]; then
+    if [ -z "$bytes_received" ]; then
+        sql_exec "insert into sys_logininfor(user_name, ipaddr, login_location, browser, os, status, msg, login_time) values('$username', '$untrusted_ip', 'openvpn', '$ifconfig_pool_remote_ip', '$ifconfig_pool_local_ip', '0', 'connected', '$log_date');"
+    else
+        sql_exec "insert into sys_logininfor(user_name, ipaddr, login_location, browser, os, status, msg, login_time) values('$username', '$untrusted_ip', 'openvpn', '[send] $bytes_received', '[recv] $bytes_sent', '0', 'disconnected', '$log_date');"
+    fi
 fi
